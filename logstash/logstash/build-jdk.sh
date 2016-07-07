f5b6ce57165e3fc553c651bc012a42e12ab78b0f
Mathew Merrick <dev@mathewmerrick.com>
2016-07-07 16:50:50 -0700

    extra

--- logstash/logstash/build-jdk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /opt/jdk
-wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.tar.gz
-tar -zxf jdk-8u91-linux-x64.tar.gz
-
-update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_91/bin/java 100
-update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_91/bin/javac 100
