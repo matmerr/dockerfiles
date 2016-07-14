f2419b532ec5a37e45030f654d25d4197e174612
Mathew Merrick <dev@mathewmerrick.com>
2016-07-14 15:49:28 -0700

    add django unattend

--- el-jefe/start.sh
+++ el-jefe/start.sh
@@ -6,11 +6,9 @@ if [ ! -f /etc/el-jefe/built.dat ]; then
         sudo -u postgres psql postgres -c "CREATE USER admin with password 'admin'" &&\
         sudo -u postgres psql postgres -c "CREATE DATABASE eljefe OWNER admin" &&\
         sudo -u postgres psql postgres -c "CREATE DATABASE cuckoo OWNER admin" &&\
+        /etc/init.d/postgresql restart
 
-    /etc/init.d/postgresql restart
-
-        python /opt/elj*/webapp/manage.py syncdb
-
+        echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@el-jefe.local', 'admin')" | ./manage.py shell
 fi
 
 /usr/bin/supervisord
