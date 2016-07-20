dd011b8da88970d39d401391ddfbc625ce3b13b3
Mathew Merrick <dev@mathewmerrick.com>
2016-07-20 14:14:21 -0700

    added el-jefe dependincies

--- el-jefe/start.sh
+++ el-jefe/start.sh
@@ -10,7 +10,7 @@ if [ ! -f /home/built.dat ]; then
         sudo -u postgres psql postgres -c "CREATE DATABASE eljefe OWNER admin" &&\
         sudo -u postgres psql postgres -c "CREATE DATABASE cuckoo OWNER admin" &&
         
-        echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@el-jefe.local', 'admin')" | ./manage.py shell
+        echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@el-jefe.local', 'admin')" | python /opt/el-jefe*/webapp/manage.py shell
 fi
 /etc/init.d/postgresql restart
 /usr/bin/supervisord
