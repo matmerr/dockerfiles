3bd2279f7d43acc3707880df08d71280ef6619d2
Mathew Merrick <m@mathewmerrick.com>
2017-11-25 23:41:01 -0800

    formatting

--- vscode-arm-qemu/setup.sh
+++ vscode-arm-qemu/setup.sh
@@ -1,5 +1,5 @@
 if [ ! -d ".vscode" ]; then
   # vscode does not exist
-  cp -R ../.vscode .
+  sudo mv ../.vscode .
 fi
 cat .vscode/terminal/.shell >> ~/.bashrc && cat .vscode/terminal/.dir_colors > ~/.dir_colors
